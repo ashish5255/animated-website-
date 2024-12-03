c52c363c5f10b1213d947c6d7e88ef5fde66e11a
Ashish <ashishadhikari5255@gmail.com>
2024-12-03 13:51:33 +0545

    Add font styles and most usable colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-    return (
-        <>
-        <main>
-            <h1 className="text-5xl text-orange-300 font-bold ">Award</h1>
-        </main>
-
-        </>
-    );
+  return (
+    <>
+      <main>
+        <h1 className="text-5xl text-orange-300 font-bold text-violet-300 ">
+          Awward
+        </h1>
+        <h2>Award</h2>
+      </main>
+    </>
+  );
 };
 
 export default App;
