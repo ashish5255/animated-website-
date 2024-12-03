e97f845964b424de5290c49356d4860af339c2ff
Ashish <ashishadhikari5255@gmail.com>
2024-12-03 13:18:56 +0545

    Add Skeleton

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+        <main>
+            <h1 className="text-5xl text-orange-300 font-bold ">Award</h1>
+        </main>
+
+        </>
+    );
+};
+
+export default App;
